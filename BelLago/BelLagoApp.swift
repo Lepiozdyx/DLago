33eb8f4e0946c3dfa419a7fafbd50e98a596c3e0
Developer <Lepiozdyx@gmail.com>
2025-08-15 11:12:23 -0300

    Initial Commit

---
+++ BelLago/BelLagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BelLagoApp.swift
+//  BelLago
+//
+//  Created by Alex on 15.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BelLagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
